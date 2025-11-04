9d3d4dc393b4e9021768f61651f48bb45bd74f44
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-04 22:14:45 +0530

    Created method to show greeting for admin

--- src/main/java/controller/DashboardContentFormController.java
+++ src/main/java/controller/DashboardContentFormController.java
@@ -1,4 +1,39 @@
 package controller;
 
-public class DashboardContentFormController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+public class DashboardContentFormController implements Initializable {
+
+    @FXML
+    private Label lblGreeting;
+
+    private void setGreetingMessage() {
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+
+        String greeting = null;
+
+        if(hour >= 5 && hour < 12) {
+            greeting = "Goog Morning Admin";
+        } else if (hour >= 12 && hour < 15) {
+            greeting = "Good Afternoon Admon";
+        } else if (hour >= 15 && hour < 21) {
+            greeting = "Good Evening Admin";
+        }else {
+            greeting = "Good Night Admin";
+        }
+
+        lblGreeting.setText(greeting);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setGreetingMessage();
+    }
 }
