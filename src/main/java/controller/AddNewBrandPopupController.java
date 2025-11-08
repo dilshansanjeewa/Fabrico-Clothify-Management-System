ed058d79fb80c34e58e013f17e1cec37aa28f3a9
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-08 22:58:27 +0530

    Created Add New Brand Pop-up & Controller Class

---
+++ src/main/java/controller/AddNewBrandPopupController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddNewBrandPopupController {
+
+    @FXML
+    private JFXButton btnCancel;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private ComboBox<?> comboSupplier;
+
+    @FXML
+    private TextField txtBrandName;
+
+    @FXML
+    void btnCancelonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveonAction(ActionEvent event) {
+
+    }
+
+}
