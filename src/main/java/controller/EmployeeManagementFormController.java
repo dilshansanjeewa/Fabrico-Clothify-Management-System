d7bc724874e4c1f432627daa8edfaa938f295c2d
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-12 23:02:42 +0530

    Redesign Employee Management form and controller

--- src/main/java/controller/EmployeeManagementFormController.java
+++ src/main/java/controller/EmployeeManagementFormController.java
@@ -1,36 +1,128 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class EmployeeManagementFormController implements Initializable {
-
-//    @FXML
-//    private ListView<?> lstSuggestions;
+public class EmployeeManagementFormController {
 
     @FXML
-    private TextField txtSearchBar;
+    private JFXButton btnCancel;
 
     @FXML
-    void txtSearchBarOnAction(ActionEvent event) {
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnSave;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnUpload;
+
+    @FXML
+    private ComboBox<?> comboDistrict;
+
+    @FXML
+    private ComboBox<?> comboProvince;
+
+    @FXML
+    private DatePicker dateDob;
+
+    @FXML
+    private ImageView imgEmployee;
+
+    @FXML
+    private ListView<?> lstSuggestions;
+
+    @FXML
+    private JFXRadioButton radioAdmin;
+
+    @FXML
+    private JFXRadioButton radioEmployee;
+
+    @FXML
+    private JFXRadioButton radioFemale;
+
+    @FXML
+    private JFXRadioButton radioMale;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnCancelonAction(ActionEvent event) {
 
     }
 
     @FXML
-    void txtSearchBarOnKeyReleased(KeyEvent event) {
+    void btnDeleteonAction(ActionEvent event) {
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        lstSuggestions.setVisible(false);
+    @FXML
+    void btnSaveonAction(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnUpdateonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUploadonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboProvinceonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void radioFemale(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchonKeyReleased(KeyEvent event) {
+
+    }
+
 }
