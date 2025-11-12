257e237fa068042d42cad2ac0aa003c2ff54a902
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-12 10:54:48 +0530

    Link FXML fx:id and onAction events with POS controller

--- src/main/java/controller/PosViewController.java
+++ src/main/java/controller/PosViewController.java
@@ -1,4 +1,150 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
+
 public class PosViewController {
+
+    @FXML
+    private JFXButton btnAllItems;
+
+    @FXML
+    private JFXButton btnCancelOrder;
+
+    @FXML
+    private JFXButton btnCheckout;
+
+    @FXML
+    private JFXButton btnClearSort;
+
+    @FXML
+    private JFXButton btnCustomers;
+
+    @FXML
+    private TableColumn<?, ?> colDescount;
+
+    @FXML
+    private TableColumn<?, ?> colItem;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private ComboBox<?> comboBrand;
+
+    @FXML
+    private ComboBox<?> comboCategory;
+
+    @FXML
+    private ComboBox<?> comboType;
+
+    @FXML
+    private FlowPane flowProducts;
+
+    @FXML
+    private ImageView imgUser;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblDescount;
+
+    @FXML
+    private Label lblNetToal;
+
+    @FXML
+    private Label lblRole;
+
+    @FXML
+    private Label lblSortOrder;
+
+    @FXML
+    private Label lblSubTotal;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblUserName;
+
+    @FXML
+    private JFXListView<?> lstSuggestions;
+
+    @FXML
+    private ScrollPane scrollProducts;
+
+    @FXML
+    private TableView<?> tblSummery;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnAllItemsonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCancelOrderonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCheckoutonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearSortonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomersonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboBrandonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboCategoryonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comboTypeonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchonKeyReleased(KeyEvent event) {
+
+    }
+
 }
