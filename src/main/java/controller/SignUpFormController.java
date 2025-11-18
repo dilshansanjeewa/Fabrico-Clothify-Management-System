84c365cdc40f70f70b874a0257415e8fceefe273
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-18 16:22:41 +0530

    Add Sign Up Form and Controller

---
+++ src/main/java/controller/SignUpFormController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SignUpFormController {
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnOtp;
+
+    @FXML
+    private JFXButton btnSelectImg;
+
+    @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
+    private JFXButton btnVerify;
+
+    @FXML
+    private Label lblWerning;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtOtp;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void btnBackinAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOtponAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSelectImgonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUponAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVerifyonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void imgOnMouseClicked(MouseEvent event) {
+
+    }
+
+}
