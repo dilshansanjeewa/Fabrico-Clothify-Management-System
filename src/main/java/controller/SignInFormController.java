3cad3ff2ba0ba8631d2374be7c984f57c1896db5
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-18 12:34:17 +0530

    Add sign in form and controller

---
+++ src/main/java/controller/SignInFormController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignInFormController {
+
+    @FXML
+    private JFXButton btnSignIn;
+
+    @FXML
+    private Hyperlink linkSignUp;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void btnSignInonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void linkSignUponAction(ActionEvent event) {
+
+    }
+
+}
