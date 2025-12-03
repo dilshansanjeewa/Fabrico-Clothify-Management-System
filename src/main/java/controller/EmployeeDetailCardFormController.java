4d0fcfb90e080b423cf54adaad07d3cad4f42d8d
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-12-03 19:57:26 +0530

    Created employee detail card and controller

---
+++ src/main/java/controller/EmployeeDetailCardFormController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class EmployeeDetailCardFormController {
+
+    @FXML
+    private ImageView imgEmployee;
+
+    @FXML
+    private Label lblDistrict;
+
+    @FXML
+    private Label lblDob;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblFirstName;
+
+    @FXML
+    private Label lblGender;
+
+    @FXML
+    private Label lblLastName;
+
+    @FXML
+    private Label lblMobile;
+
+    @FXML
+    private Label lblPostalCode;
+
+    @FXML
+    private Label lblProvince;
+
+    @FXML
+    private Label lblStreetAddress;
+
+    @FXML
+    private Label lblVisibleId;
+
+}
