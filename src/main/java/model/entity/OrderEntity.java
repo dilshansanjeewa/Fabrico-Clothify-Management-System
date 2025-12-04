a593f5d0ef535792034ea2a827f2f7c6fa86bd7e
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-12-04 12:15:22 +0530

    Created order,order detail DTOs and Entities

---
+++ src/main/java/model/entity/OrderEntity.java
@@ -0,0 +1,35 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+@Entity
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "order_code", unique = true, nullable = false)
+    private String orderCode;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDate orderDate;
+
+    @Column(name = "total_amount", nullable = false)
+    private double totalAmount;
+
+    @Column(name = "order_details", nullable = false)
+    @OneToMany(mappedBy = "orderEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderDetailEntity> orderDetailEntities;
+}
